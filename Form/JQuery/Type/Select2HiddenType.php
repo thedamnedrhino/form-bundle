bfd02f4f79a33e34fb9a99ec0c8c8d7146a6709c
thedamnedrhino <farzadsbakhtiar@gmail.com>
2017-07-08 13:55:07 +0430

    Update Select2HiddenType.php

--- Form/JQuery/Type/Select2HiddenType.php
+++ Form/JQuery/Type/Select2HiddenType.php
@@ -57,7 +57,7 @@ class Select2HiddenType extends AbstractType
         // Adds a custom block prefix
         array_splice(
             $view->vars['block_prefixes'],
-            array_search($this->getName(), $view->vars['block_prefixes']),
+            array_search($this->getBlockPrefix(), $view->vars['block_prefixes']),
             0,
             'genemu_jqueryselect2'
         );
